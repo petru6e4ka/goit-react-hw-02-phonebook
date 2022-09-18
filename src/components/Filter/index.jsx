e969bb84f50733412793bd4c2aa623a55580334c
anastasiia.vorobiova <anastasiiavorobiova2403@gmail.com>
2022-09-19 02:01:20 +0300

    phonebook

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { Input, FilterBlock } from '../styles';
+
+export const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <FilterBlock>
+      <p>Find contacts by name</p>
+      <Input
+        type="text"
+        name="filter"
+        onChange={handleFilterChange}
+        value={filter}
+      />
+    </FilterBlock>
+  );
+};
+
+Filter.propTypes = {
+  filter: PropTypes.string,
+  handleFilterChange: PropTypes.func,
+};
